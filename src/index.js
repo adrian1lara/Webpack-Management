f2ebda1d131c242b1609884a89acbd2895cd21a2
adrian1lara <adrianlara203@gmail.com>
2023-08-16 15:01:04 -0500

    webpack

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import _ from 'lodash';
+import printMe from './print';
+
+function component() {
+    const element = document.createElement('div');
+    const btn = document.createElement('button');
+  
+    element.textContent = _.join(['Hello', 'webpack'], ' ');
+    btn.textContent = 'Click me and check the console!';
+    btn.onclick = printMe;
+
+    element.appendChild(btn);
+    return element;
+  }
+  
+  document.body.appendChild(component());
